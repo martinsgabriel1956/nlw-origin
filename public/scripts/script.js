754887c2cc47daa72f8e242ab6be701b5e380788
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-05-28 12:42:24 -0300

    feat: fourth day finished

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,16 +1,24 @@
-const userLinks = {
-  github: "martinsgabriel1956",
-  youtube: "UCLJWFd14kT4FQReMjowBkjQ",
-  instagram: "martinsgabriel99",
-  twitter: "martinsgabriel99",
-  facebook: "martinsbiel1956",
-}
+import { userSocialLinks } from './utils.js';
 
 const changeSocialMediaLinks = (() => {
   for(let li of socialLinks.children) {
     const social = li.getAttribute("class");
 
-    li.children[0].href = `https://www.${social}.com/${social === 'youtube' ? 'channel/' : ''}${userLinks[social]}`;
+    li.children[0].href = `https://www.${social}.com/${social === 'youtube' ? 'channel/' : ''}${userSocialLinks[social]}`;
   }
 })()
 
+const getGithubUserInfo = (async () => {
+  const url = `https://api.github.com/users/${userSocialLinks.github}`;
+
+  const response = await fetch(url);
+  const userData = await response.json();
+
+  const { name, bio, avatar_url, login } = userData;
+
+  avatar.src = avatar_url;
+  avatar.alt = `Avatar do ${name}`;
+  userName.textContent = name;
+  userLogin.textContent = login;
+  userBio.textContent = bio;
+})()
---
+++ public/scripts/utils.js
@@ -0,0 +1,7 @@
+export const userSocialLinks = {
+  github: "martinsgabriel1956",
+  youtube: "UCLJWFd14kT4FQReMjowBkjQ",
+  instagram: "martinsgabriel99",
+  twitter: "martinsgabriel99",
+  facebook: "martinsbiel1956",
+}
